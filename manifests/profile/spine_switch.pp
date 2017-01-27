da87b1ded595f34a3c3d022f5bafa2f512a9908f
Chris Barker <cbarker@puppetlabs.com>
2017-01-26 21:13:56 -0800

    update for formating and puppet 4.8 requirement

--- manifests/profile/spine_switch.pp
+++ manifests/profile/spine_switch.pp
@@ -1,15 +1,15 @@
 class evpn_vxlan::profile::spine_switch{
 
   #1. Enabling the features required
-       include evpn_vxlan::enable_features::spine
+  include evpn_vxlan::enable_features::spine
   #2. configure underlay interfaces
-       include evpn_vxlan::underlay_interface
+  include evpn_vxlan::underlay_interface
   #3. configure  ospf
-       include evpn_vxlan::ospf
+  include evpn_vxlan::ospf
   #4. configure bgp
-       include evpn_vxlan::bgp::spine
+  include evpn_vxlan::bgp::spine
   #5. configure pim
-      include evpn_vxlan::pim::spine
+  include evpn_vxlan::pim::spine
 
   Class['evpn_vxlan::underlay_interface'] ->
   Class['evpn_vxlan::ospf']
